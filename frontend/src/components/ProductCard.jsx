63570315cd09df56807d48036d82efb482bf744d
rahmanwolied <mosheur.r.wolied@gmail.com>
2023-08-15 06:35:43 +0600

    woking on image upload

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line react/prop-types
 const ProductCard = ({ product }) => {
 	return (
 		<div className="card w-96 bg-base-100 border-solid border border-[rgba(18, 18, 18, 0.10)]">
@@ -5,12 +6,23 @@ const ProductCard = ({ product }) => {
 				<img src="images/fruit6.png" alt="Shoes" className="rounded-xl" />
 			</figure> */}
 			<div className="card-body items-center text-center">
-				<h2 className="card-title">{product.name}</h2>
+				<h2 className="card-title">
+					{
+						// eslint-disable-next-line react/prop-types
+						product.name
+					}
+				</h2>
 				<div className="divider"></div>
-				<p>${product.price}</p>
+				<p>
+					$
+					{
+						// eslint-disable-next-line react/prop-types
+						product.price
+					}
+				</p>
 
 				<div className="card-actions">
-					<button className="btn btn-link text-[#f85559] no-underline" onclick="modal1.showModal()">
+					<button className="btn btn-link text-[#f85559] no-underline" onClick="modal1.showModal()">
 						Details
 					</button>
 				</div>
